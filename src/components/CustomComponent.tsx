b1f6c177ed2d1418529044cd5b26faa65b1cbda2
altemir.mad <altemir.mad@gmail.com>
2024-12-21 20:59:31 +0500

    adssad

--- src/components/CustomComponent.tsx
+++ src/components/CustomComponent.tsx
@@ -1,11 +1,57 @@
-import { useSendTransaction, useWriteContract, useAccount  } from 'wagmi'
-import { parseEther, parseUnits } from 'viem'
-import  abi  from '../../abi.json'
+import { useSendTransaction, useWriteContract, useAccount, useReadContract, useWaitForTransactionReceipt   } from 'wagmi'
+import { parseEther, parseUnits, parseAbi } from 'viem'
+import  abiSale  from '../../abi.json'
+import  usdtPolAbi  from '../../usdtPolAbi.json'
+import { config } from "../../config"
+import { waitForTransactionReceipt } from '@wagmi/core'
+
 
 export const CustomComponent = () => {
   const { sendTransaction } = useSendTransaction()
-  const { writeContract } = useWriteContract()
+  const { writeContract, data: hash, isPending, writeContractAsync } = useWriteContract()
   const { address } = useAccount()
+  const result = useReadContract({
+    abi: abiSale,
+    address: '0x24689DC23088E84ad17537E92717a4928474eCAF',
+    functionName: 'getUserTokens',
+    args: [1, address]
+})
+
+const buyTokens = async () => {
+    try {
+        const result = await writeContractAsync({
+            abi: usdtPolAbi,
+            address: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
+            functionName: 'approve',
+            args: [
+                '0x24689DC23088E84ad17537E92717a4928474eCAF',
+                parseUnits("1", 6),
+            ],
+        })
+        console.log(result)
+        const receipt1 = await waitForTransactionReceipt(config, {hash: result})
+
+        const result2 = writeContractAsync({
+            abi: abiSale,
+            address: '0x24689DC23088E84ad17537E92717a4928474eCAF',
+            functionName: 'buyTokens',
+            args: [
+              address,
+              1,
+              parseUnits("1", 6),
+              '0x0000000000000000000000000000000000000000'
+            ],
+        })
+        console.log(result2)
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
+
+
+  let balance
 
     return (
         <>
@@ -20,21 +66,11 @@ export const CustomComponent = () => {
             </button>
             <p>{address}</p>
             <button onClick={() =>
-                writeContract({
-                    abi,
-                    address: '0x24689DC23088E84ad17537E92717a4928474eCAF',
-                    functionName: 'buyTokens',
-                    args: [
-                      address,
-                      1,
-                      parseUnits("1", 6),
-                      '0x0000000000000000000000000000000000000000'
-                    ],
-                })
+                buyTokens()
             }>
             Smart contract write
             </button>
+            <p></p>
         </>
     )
 }
-
